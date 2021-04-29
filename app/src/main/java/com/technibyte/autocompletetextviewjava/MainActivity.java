fd5d7a62c729cd3b86f9a930926aba9b1eea5371
aleangelozi <aleangelozi@gmail.com>
2021-04-28 22:22:49 -0300

    Replacing findViewById by View Binding.

--- app/src/main/java/com/technibyte/autocompletetextviewjava/MainActivity.java
+++ app/src/main/java/com/technibyte/autocompletetextviewjava/MainActivity.java
@@ -1,21 +1,28 @@
 package com.technibyte.autocompletetextviewjava;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.technibyte.autocompletetextviewjava.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         String[] cities = getResources().getStringArray(R.array.Cities);
 
-        AutoCompleteTextView suggestions = findViewById(R.id.autoCompleteTextView);
+        AutoCompleteTextView suggestions = binding.autoCompleteTextView;
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_dropdown_item_1line, cities);
